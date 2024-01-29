799377d78c59756a1732e157ec0369aeeeea6f4d
sajanrajdev <sajanrajdev10@gmail.com>
2024-01-29 16:37:32 -0700

    feat: modify script to fetch inital data

--- scripts/fetch_cl_data.py
+++ scripts/fetch_cl_data.py
@@ -1,34 +1,27 @@
 from ape import Contract
 import pandas as pd
 
+# Examples:
+
 # ETH/BTC: https://data.chain.link/feeds/ethereum/mainnet/eth-btc
-# LATEST_ROUND = 18446744073709572902
-# LASTEST_UPDATE = 1706555999
 # START_TIME = 1643511664 # Unix timestamp for the oldest round to fetch
 # FEED_ADDRESS = "0xAc559F25B1619171CbC396a50854A3240b6A4e99"
 
 # stETH/ETH: https://data.chain.link/feeds/ethereum/mainnet/steth-eth
-# LATEST_ROUND = 18446744073709552519
-# LASTEST_UPDATE = 1706484887
 # START_TIME = 1643511664 # Unix timestamp for the oldest round to fetch
 # FEED_ADDRESS = "0x86392dC19c0b719886221c78AB11eb8Cf5c52812"
 
 # BTC/USD: https://data.chain.link/feeds/ethereum/mainnet/steth-eth
-# LATEST_ROUND = 110680464442257320387
-# LASTEST_UPDATE = 1706558135
 # START_TIME = 1643511664 # Unix timestamp for the oldest round to fetch
 # FEED_ADDRESS = "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c"
 
 # ETH/USD: https://data.chain.link/feeds/ethereum/mainnet/eth-usd
-LATEST_ROUND = 110680464442257320802
-LASTEST_UPDATE = 1706558123
 START_TIME = 1643511664 # Unix timestamp for the oldest round to fetch
 FEED_ADDRESS = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
 
 def main():
     feed = Contract(FEED_ADDRESS, abi="abi.json")
-    latest_round = LATEST_ROUND
-    last_time = LASTEST_UPDATE
+    (latest_round, answer, start, last_time, answeredInRound) = feed.latestRoundData()
 
     feed_data = []
     while (last_time > START_TIME):
